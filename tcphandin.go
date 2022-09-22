361a8b061c93e20116258603ab269787bc46407a
katrinesando <kmsa@itu.dk>
2022-09-22 14:10:17 +0200

    commit with handshake (not finished) to be able to pull

--- tcphandin.go
+++ tcphandin.go
@@ -1,11 +1,18 @@
 package main
 
+import "math/rand"
+
 type packet struct {
 	source      uint16
 	destination uint16
 	sequenceNum uint32
 	checksum    uint16
 	data        [4]byte
+}
+
+type Pair[T, U any] struct {
+	First  T
+	Second U
 }
 
 func PacketHash(p packet) uint16 {
@@ -29,7 +36,36 @@ func IntPow(base uint16, exp int) uint16 {
 	return result
 }
 
-func Main() {
+func Host(name string, syn chan Pair[int, int], ack chan int, packetchan chan packet) {
 
-	channel := make(chan packet)
 }
+
+func threeWayHandshakeClient(syncChan [2]chan int, ackChan [2]chan int) {
+	initSeq := rand.Intn(2-1) + 1 //number between 1-2
+	syncChan[0] <- 1
+	syncChan[1] <- initSeq
+
+	if 1 == 1 {
+
+	} else {
+		//try again
+	}
+}
+
+func threeWayHandshakeServer(syncChan [2]chan int, ackChan [2]chan int) {
+	initSeq := rand.Intn(2-1) + 1 //number between 1-2
+	seqRecived := <-syncChan[1]
+	if initSeq == seqRecived {
+		ackChan[0] <- 1 //can establish contact
+		ackChan[1] <- initSeq + 1
+	} else {
+		ackChan[0] <- 0 //cannot establish contact
+	}
+}
+
+func Main() {
+	channel := make(chan packet)
+	tup := tuple.New2(5, "hi!")
+}
+
+//server kører herinde
